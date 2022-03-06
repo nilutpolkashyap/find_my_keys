522a15fe4d889da7b46351a0ba4cb143aa1ea23a
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-03-06 23:31:47 +0530

    Update find_my_keys.ino

--- find_my_keys.ino
+++ find_my_keys.ino
@@ -6,66 +6,73 @@ char Incoming_value = 0;
 
 BluetoothSerial bluetooth;
 
-extern const unsigned char previewR[120264];  
+extern const unsigned char previewR[120264];  // Referring to external data (Dingdong audio files are stored inside).   引用外部数据（dingdong音频文件存储在内）
 
 
 
-#define CONFIG_I2S_BCK_PIN 12 
+#define CONFIG_I2S_BCK_PIN 12 //定义I2S相关端口
 #define CONFIG_I2S_LRCK_PIN 0
 #define CONFIG_I2S_DATA_PIN 2
 #define CONFIG_I2S_DATA_IN_PIN 34
 
-#define Speak_I2S_NUMBER I2S_NUM_0  
+#define Speak_I2S_NUMBER I2S_NUM_0  //定义扬声器端口
 
-#define MODE_MIC 0  
+#define MODE_MIC 0  //定义工作模式
 #define MODE_SPK 1
 #define DATA_SIZE 1024
 
-bool InitI2SSpeakOrMic(int mode){  
+bool InitI2SSpeakOrMic(int mode){  //Init I2S.  初始化I2S
     esp_err_t err = ESP_OK;
 
-    i2s_driver_uninstall(Speak_I2S_NUMBER); 
+    i2s_driver_uninstall(Speak_I2S_NUMBER); // Uninstall the I2S driver.  卸载I2S驱动
     i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER), 
-        .sample_rate = 44100, 
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, 
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT, 
-        .communication_format = I2S_COMM_FORMAT_I2S,  
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, 
-        .dma_buf_count = 2, 
-        .dma_buf_len = 128, 
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER),  // Set the I2S operating mode.  设置I2S工作模式
+        .sample_rate = 44100, // Set the I2S sampling rate.  设置I2S采样率
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // Fixed 12-bit stereo MSB.  固定为12位立体声MSB
+        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT, // Set the channel format.  设置频道格式
+        .communication_format = I2S_COMM_FORMAT_I2S,  // Set the format of the communication.  设置通讯格式
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // Set the interrupt flag.  设置中断的标志
+        .dma_buf_count = 2, //DMA buffer count.  DMA缓冲区计数
+        .dma_buf_len = 128, //DMA buffer length.  DMA缓冲区长度
     };
     if (mode == MODE_MIC){
         i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM);
     }else{
         i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX);
-        i2s_config.use_apll = false;  
-        i2s_config.tx_desc_auto_clear = true; 
+        i2s_config.use_apll = false;  //I2S clock setup.  I2S时钟设置
+        i2s_config.tx_desc_auto_clear = true; // Enables auto-cleanup descriptors for understreams.  开启欠流自动清除描述符
     }
+    // Install and drive I2S.  安装并驱动I2S
     err += i2s_driver_install(Speak_I2S_NUMBER, &i2s_config, 0, NULL);
 
     i2s_pin_config_t tx_pin_config;
-    tx_pin_config.bck_io_num = CONFIG_I2S_BCK_PIN;  
-    tx_pin_config.ws_io_num = CONFIG_I2S_LRCK_PIN;  
-    tx_pin_config.data_out_num = CONFIG_I2S_DATA_PIN; 
-    tx_pin_config.data_in_num = CONFIG_I2S_DATA_IN_PIN; 
-    err += i2s_set_pin(Speak_I2S_NUMBER, &tx_pin_config); 
-    err += i2s_set_clk(Speak_I2S_NUMBER, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO); 
+    tx_pin_config.bck_io_num = CONFIG_I2S_BCK_PIN;  // Link the BCK to the CONFIG_I2S_BCK_PIN pin. 将BCK链接至CONFIG_I2S_BCK_PIN引脚
+    tx_pin_config.ws_io_num = CONFIG_I2S_LRCK_PIN;  //          ...
+    tx_pin_config.data_out_num = CONFIG_I2S_DATA_PIN;  //       ...
+    tx_pin_config.data_in_num = CONFIG_I2S_DATA_IN_PIN; //      ...
+    err += i2s_set_pin(Speak_I2S_NUMBER, &tx_pin_config); // Set the I2S pin number.  设置I2S引脚编号
+    err += i2s_set_clk(Speak_I2S_NUMBER, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO); // Set the clock and bitwidth used by I2S Rx and Tx. 设置I2S RX、Tx使用的时钟和位宽
     return true;
 }
 
-void DisplayInit(void){ 
-  M5.Lcd.fillScreen(WHITE); 
-  M5.Lcd.setTextColor(BLACK); 
-  M5.Lcd.setTextSize(2);  
+void DisplayInit(void){ // Initialize the display. 显示屏初始化
+  M5.Lcd.fillScreen(WHITE); //Set the screen background color to white.  设置屏幕背景色为白色
+  M5.Lcd.setTextColor(BLACK); //Set the text color to black.  设置文字颜色为黑色
+  M5.Lcd.setTextSize(2);  //Set font size to 2.  设置字体大小为2
 }
 
-void SpeakInit(void){ 
-  M5.Axp.SetSpkEnable(true); 
+void SpeakInit(void){ // 初始化扬声器
+  M5.Axp.SetSpkEnable(true);  //启用扬声器电源
   InitI2SSpeakOrMic(MODE_SPK);
 }
 
-
+void DisplayText(void){
+  M5.Lcd.drawString("Connect to Bluetooth", 10, 90, 2);
+//  M5.Lcd.print("Device ON\n");
+//  M5.Lcd.print("Bluetooth ON");
+  M5.Lcd.drawString("FIND MY KEYS", 40, 10, 2);
+  M5.Lcd.drawString("Bluetooth ON", 10, 60, 2);
+}
 
 void DingDong(void){
   size_t bytes_written = 0;
@@ -78,26 +85,28 @@ void setup()
   M5.Lcd.begin(); 
   M5.Lcd.setTextSize(4);
   DisplayInit();
-
-  M5.Lcd.drawString("Device ON", 20, 15, 3);
-
-  M5.Lcd.drawString("FIND MY KEYS", 40, 50, 3);
-  M5.Lcd.print("Device ON\n");
-  M5.Lcd.print("Bluetooth ON");
-  M5.Lcd.drawString("FIND MY KEYS", 40, 50, 3);
+//  M5.Lcd.print("Device ON");
+//  M5.Lcd.drawString("Device ON", 20, 15, 3);
+  M5.Lcd.drawString("Connect to Bluetooth", 10, 90, 2);
+//  M5.Lcd.print("Device ON\n");
+//  M5.Lcd.print("Bluetooth ON");
+  M5.Lcd.drawString("FIND MY KEYS", 40, 10, 2);
+  M5.Lcd.drawString("Bluetooth ON", 10, 60, 2);
   
 SpeakInit();
   DingDong();
   
   Serial.begin(9600);         
-  pinMode(14, OUTPUT);    
-  pinMode(25, OUTPUT);
+//  pinMode(14, OUTPUT);    
+//  pinMode(25, OUTPUT);
 
-  digitalWrite(25, HIGH);
+//  digitalWrite(25, HIGH);
 
-  bluetooth.begin("ESP32 Bluetooth");
+  bluetooth.begin("FIND MY KEYS");
   bluetooth.println("Bluetooth ready");
 }
+
+int ab = 0;
 
 void loop()
 {
@@ -108,13 +117,29 @@ void loop()
     Serial.print("\n");        
     if(Incoming_value == '1')    
     {         
-      digitalWrite(14, HIGH);
-      M5.Axp.SetLDOEnable(3,true);  
-      delay(100);
-      M5.Axp.SetLDOEnable(3,false);
-      DingDong();
+//      digitalWrite(14, HIGH);
+//      digitalWrite(25, HIGH);
+      ab = 0;
+      while(ab<=5)
+      {
+        M5.Lcd.fillScreen(RED);
+        DisplayText();
+        M5.Axp.SetLDOEnable(3,true);  
+        delay(100);
+        M5.Axp.SetLDOEnable(3,false);
+        DingDong();
+        ab++;
+        M5.Lcd.fillScreen(ORANGE);
+        DisplayText();
+        delay(100);
+      }
+      
     }
     else if(Incoming_value == '0')       
-      digitalWrite(14, LOW);   
+//      digitalWrite(14, LOW);   
+      M5.Lcd.fillScreen(WHITE);
+      DisplayText();
+      delay(50);
+//      digitalWrite(25, LOW);
   }                            
 } 
